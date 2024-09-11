b71f5ff3470e6f4cef5cce221f6071bfcdfec707
Michael Hall <michael@mbh.sh>
2024-09-11 17:00:53 +1000

    chore: fmt

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,5 +1,6 @@
-from fastq_dl.download import get_sra_metadata, get_ena_metadata
 import pytest
+
+from fastq_dl.download import get_ena_metadata, get_sra_metadata
 
 
 @pytest.mark.filterwarnings("ignore::DeprecationWarning")
@@ -13,12 +14,14 @@ def test_get_sra_metadata_success():
     assert metadata["run_accession"] == accession
     assert metadata["study_accession"] == "SRP065366"
 
+
 @pytest.mark.filterwarnings("ignore::DeprecationWarning")
 def test_get_sra_metadata_failure():
     accession = "SRR0000000"
     success, metadata = get_sra_metadata(accession)
     assert not success
     assert not metadata
+
 
 def test_get_ena_metadata_success():
     accession = "SRR2838701"
@@ -29,6 +32,7 @@ def test_get_ena_metadata_success():
 
     assert metadata["run_accession"] == accession
     assert metadata["sample_accession"] == "SAMN04215065"
+
 
 def test_get_ena_metadata_failure():
     accession = "SRR0000000"
